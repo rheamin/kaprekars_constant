daf8c7cd7bbc794ea42d345779086afa179f45a3
Rhea Minnaar <rheamin11@proton.me>
2026-01-11 13:13:39 -0700

    user can select number of times to run the program

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-// A simple program to test kaprekars constant 
+// A simple program to test kaprekars constant
 
+use std::io::{Write, stdin, stdout};
 
 fn main() {
     println!("Kaprekar's Constant demo in Rust");
-    
-    // generate random 4 digit number
-    let rng = rand::random_range(1000..9999);
-    let it = kaprekar_step(rng);
-    println!("{rng} took {it} steps to reach 6174");
+
+    // ask the user how many times to run
+    let count = prompt_number("How many numbers would you like to try".to_string());
+
+    for _ in 0..count {
+        // generate random 4 digit number
+        let rng = rand::random_range(1000..9999);
+        println!("Testing {rng}");
+        let it = kaprekar_step(rng);
+        println!("{rng} took {it} steps to reach 6174");
+    }
 }
 
 fn kaprekar_step(n: u32) -> u32 {
@@ -25,20 +32,19 @@ fn kaprekar_step(n: u32) -> u32 {
 }
 
 fn digit_sort_sm(n: u32) -> u32 {
-    
     let mut v = num_to_vec(n);
-    v.sort(); 
-    
+    v.sort();
+
     // convert vec back to number
     let t = v.iter().fold("".to_string(), |acc, x| acc + &x.to_string());
     return t.parse::<u32>().unwrap();
 }
- 
+
 fn digit_sort_lg(n: u32) -> u32 {
     let mut v = num_to_vec(n);
-    v.sort(); 
+    v.sort();
     v.reverse();
-    
+
     // convert vec back to number
     let t = v.iter().fold("".to_string(), |acc, x| acc + &x.to_string());
     return t.parse::<u32>().unwrap();
@@ -46,7 +52,34 @@ fn digit_sort_lg(n: u32) -> u32 {
 
 fn num_to_vec(n: u32) -> Vec<u32> {
     n.to_string()
-            .chars()
-            .map(|c| c.to_digit(10).unwrap())
-            .collect()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect()
+}
+
+fn prompt_number(msg: String) -> i32 {
+    let mut is_valid = false;
+    let mut num = 0;
+
+    let mut s = String::new();
+
+    while !is_valid {
+        println!("{msg} ");
+
+        let _ = stdout().flush();
+        stdin().read_line(&mut s).expect("Did not enter a string");
+
+        // validate input
+        num = match s.trim().parse() {
+            Ok(val) => {
+                is_valid = true;
+                val
+            }
+            Err(err) => {
+                println!("Invalid String: {err}");
+                0
+            }
+        };
+    }
+    return num;
 }
